212befa665f2e788a61abe39070549a1abba9913
bilal-tareq <bilaltareq2005@gmail.com>
2025-08-21 22:17:58 +0400

    the second page style finsh

--- scripts/order.js
+++ scripts/order.js
@@ -1,10 +1,28 @@
- function showStars(num) {
-    const starsContainer = document.getElementById('starsContainer');
-    let stars = "";
+  function showStars(num) {
+      const starsContainer = document.getElementById('starsContainer');
+      starsContainer.innerHTML = "";
+      
+      for (let i = 0; i < num; i++) {
+        const star = document.createElement("img");
+        star.src = "images/icon-star.svg";
+        star.className = "star";
+        star.style.width = "30px";
+        star.style.height = "30px";
 
-    for (let i = 0; i < num; i++) {
-     stars += "★ ";
+        star.alt = "Star";
+        starsContainer.appendChild(star);
+      }
     }
+function submitRating() {
 
-    starsContainer.innerHTML = stars;
+    const starsContainer = document.getElementById('starsContainer');
+    const rating = starsContainer.children.length;
+      
+    if (rating > 0) {
+        alert(`You rated us ${rating} star(s)!`);
+    } else {
+        alert("Please select a rating before submitting.");
+    }
 }
+
+
